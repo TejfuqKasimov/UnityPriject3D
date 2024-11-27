899b20195fc20af67caf842ce241b23626a8adbb
Tofiq <tekasimov@edu.hse.ru>
2024-11-27 03:06:13 +0300

    #add "way" prefab and script for finding the coordinates of the character

--- Assets/Scripts/Generate Blocks/MazeSpawner.cs
+++ Assets/Scripts/Generate Blocks/MazeSpawner.cs
@@ -12,33 +12,44 @@ public class MazeSpawner : MonoBehaviour
     private GameObject previousPrBlock;
     public GameObject startRoomPrefab;
     private GameObject startRoom;
-    private GameObject[,] currentGOMaze = new GameObject[11, 11];
-    private GameObject[,] previousGOMaze = new GameObject[11, 11];
-    private GameObject[,] previousPrGOMaze = new GameObject[11, 11];
     private float Rast = 5.5f;
     private float PlayerRast = 5.5f;
+    private float PlayerPrRast = 5.5f;
     private const float Wight = 3.4f;
     private const float Height = 3.4f;
     private int CountWight = 5;
     private int CountHeight = 5;
-    public Transform obj;
+    private GameObject[,] currentGOMaze = new GameObject[5, 5];
+    private GameObject[,] previousGOMaze = new GameObject[5, 5];
+    private GameObject[,] previousPrGOMaze = new GameObject[5, 5];
+    public Transform Character;
     private int LevelCount = 0;
+    [SerializeField] private bool isEnd = false;
+    public GameObject WayPrefab;
+    private GameObject[,] currentPath = new GameObject[11, 11];
+    private GameObject[,] previousPath = new GameObject[11, 11];
+    private GameObject[,] previousPrPath = new GameObject[11, 11];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startRoom = Instantiate(startRoomPrefab, Vector3.zero, quaternion.identity);
         currentBlock = createBlock();
+        currentPath = GenerateWay();
         currentGOMaze = Generate();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (obj.position.x >= PlayerRast)
+        if (Character.position.x >= PlayerRast)
         {
+            PlayerPrRast = PlayerRast;
             PlayerRast = Rast;
             LevelCount++;
+            previousPrPath = previousPath;
+            previousPath = currentPath;
+            currentPath = GenerateWay();
             previousPrGOMaze = previousGOMaze;
             previousGOMaze = currentGOMaze;
             currentGOMaze = Generate();
@@ -66,8 +77,19 @@ public class MazeSpawner : MonoBehaviour
                     {
                         Destroy(previousPrGOMaze[x, z]);
                     }
+                    if (previousPrPath[x, z] != null)
+                    {
+                        Destroy(previousPrPath[x, z]);
+                    }
                 }
             }
+        }
+        if (isEnd)
+        {
+            int x = (int)((Character.position.x - PlayerPrRast) / Height);
+            int y = (int)((Character.position.z + (Height * previousPrGOMaze.GetLength(0) / 2)) / Height);
+            Debug.Log(x + ", " + y);
+            // ShowWay();
         }
     }
     public GameObject[,] Generate()
@@ -75,17 +97,49 @@ public class MazeSpawner : MonoBehaviour
         MazeGenerator generator = new MazeGenerator();
         MazeGeneratorCell[,] maze = generator.GenerateMaze(CountWight, CountHeight);
         GameObject[,] GOMaze = new GameObject[maze.GetLength(0), maze.GetLength(1)];
+        currentPath[0, CountHeight / 2].GetComponent<Way>().Bottom.SetActive(true);
         for (int x = 0; x < maze.GetLength(0); ++x)
         {
             for (int z = 0; z < maze.GetLength(1); ++z)
             {
                 GOMaze[x, z] = Instantiate(cellPrefab, new Vector3(PlayerRast + 1.7f + x * Wight, 0, -(Height * CountHeight / 2 - 1.7f) + z * Height), quaternion.identity);
+                
                 Cell c = GOMaze[x, z].GetComponent<Cell>();
                 c.WallLeft.SetActive(maze[x, z].WallLeft);
                 c.WallBottom.SetActive(maze[x, z].WallBottom);
+                if (maze[x, z].WallLeft) currentPath[x, z].GetComponent<Way>().Left.SetActive(false);
+                else                     currentPath[x, z].GetComponent<Way>().Left.SetActive(true);
+
+                if (maze[x, z].WallBottom) currentPath[x, z].GetComponent<Way>().Up.SetActive(false);
+                else                       currentPath[x, z].GetComponent<Way>().Up.SetActive(true);
+
+                if (x > 0)
+                    if (maze[x - 1, z].WallBottom) currentPath[x, z].GetComponent<Way>().Bottom.SetActive(false);
+                    else                         currentPath[x, z].GetComponent<Way>().Bottom.SetActive(true);
+                
+                if (z > 0)
+                    if (maze[x, z - 1].WallLeft) currentPath[x, z].GetComponent<Way>().Right.SetActive(false);
+                    else                           currentPath[x, z].GetComponent<Way>().Right.SetActive(true);
             }
         }
         return GOMaze;
+    }
+    public GameObject[,] GenerateWay()
+    {
+        GameObject[,] WayObj = new GameObject[CountHeight, CountWight];
+        for (int x = 0; x < CountHeight; ++x)
+        {
+            for (int z = 0; z < CountWight; ++z)
+            {
+                WayObj[x, z] = Instantiate(WayPrefab, new Vector3(PlayerRast + 1.7f + x * Wight, 0, -(Height * CountHeight / 2 - 1.7f) + z * Height), quaternion.identity);
+                Way c = WayObj[x, z].GetComponent<Way>();
+                c.Left.SetActive(false);
+                c.Right.SetActive(false);
+                c.Up.SetActive(false);
+                c.Bottom.SetActive(false);
+            }
+        }
+        return WayObj;
     }
     private GameObject createBlock() 
     {
@@ -114,4 +168,9 @@ public class MazeSpawner : MonoBehaviour
 
         return curObj;
     }
+
+    // private void ShowWay()
+    // {
+        
+    // }
 }
---
+++ Assets/Scripts/Generate Blocks/Way.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Way : MonoBehaviour
+{
+    public GameObject Left;
+    public GameObject Right;
+    public GameObject Bottom;
+    public GameObject Up;
+}
