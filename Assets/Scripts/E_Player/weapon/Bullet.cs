30b47ac58852e6e4cdfc32f7b84e4e1e4718be08
Tofiq <tekasimov@edu.hse.ru>
2024-12-19 00:45:31 +0300

    #fix bullet and scene

--- Assets/Scripts/E_Player/weapon/Bullet.cs
+++ Assets/Scripts/E_Player/weapon/Bullet.cs
@@ -4,11 +4,16 @@ public class Bullet : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private float Damage = 25.0f;
+    public GameObject player;
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Damage -= 0.2f;
+        if (Vector3.Distance(transform.position, GameObject.FindGameObjectsWithTag("Player")[0].transform.position) > 100f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
